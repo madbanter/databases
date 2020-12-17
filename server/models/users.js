87c450cf832b30f647f5d739f85f2bd0e0d534cd
Adam Brent <adam.brent@gmail.com>
2020-12-17 11:25:32 -0800

    Adds functions in users models.

--- server/models/users.js
+++ server/models/users.js
@@ -1,6 +1,24 @@
 var db = require('../db');
 
 module.exports = {
-  getAll: function () {},
-  create: function () {}
+  getAll: function () {
+    db.query('SELECT * FROM users', (err, results) => {
+      if (err) {
+        throw error;
+      } else {
+        return results;
+      }
+    });
+  },
+  create: function (user) {
+    var username = user.username || '';
+    var sqlCommand = `INSERT INTO users (name) VALUES (${username})`;
+    db.query(mysqlCommand, (err, result) => {
+      if (err) {
+        throw err;
+      } else {
+        console.log('Username inserted');
+      }
+    });
+  }
 };
